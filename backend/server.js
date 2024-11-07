c16e7891ff2c4bf8aca563aad7d43e17dc211f5d
ashutosh86941 <113468524+ashutosh86941@users.noreply.github.com>
2024-11-08 00:30:37 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import Process from "process";
 import dotenv from "dotenv";
@@ -14,11 +15,16 @@ connectDB();
 dotenv.config();
 
 app.use(cookieParser());
-const port = Process.env.PORT;
+const port = Process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users" , userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist" , "index.html"));
+})
 
 server.listen(port, () => {});
